7294132cd7ae8eb8006e084681165443236bf82d
moses966 <mosikoproducts@gmail.com>
2025-01-03 21:26:08 +0300

    Added tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,203 @@
-import  os
+import os
 import sys
 import pytest
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 from scripts import constants
 
+class Sandbox:
+    def __init__(self, project, deployer):
+        self.project = project
+        self.deployer = deployer
+        self._contracts = {}  # Dictionary to cache deployed contracts
+
+    def deploy_finder_contract(self):
+        if "finder_contract" not in self._contracts:
+            self._contracts["finder_contract"] = self.deployer.deploy(self.project.FinderContract)
+        #return self._contracts["finder_contract"]
+
+    def deploy_store_contract(self):
+        if "store_contract" not in self._contracts:
+            self._contracts["store_contract"] = self.deployer.deploy(
+                self.project.StoreContract,
+                constants.fixed_oracle_fee,
+                constants.weekly_delay_fee,
+                constants.empty_address,
+            )
+        #return self._contracts["store_contract"]
+    
+    def deploy_ancillary_interface(self):
+        if "ancillary_interface" not in self._contracts:
+            self._contracts["ancillary_interface"] = self.deployer.deploy(
+                self.project.AncillaryDataInterface
+            )
+
+    def deploy_currency(self):
+        if "currency" not in self._contracts:
+            self._contracts["currency"] = self.deployer.deploy(
+                self.project.TestERC20,
+                "Test ERC20",
+                "TT",
+                18
+            )
+        #return self._contracts["currency"]
+
+    def deploy_address_whitelist(self):
+        if "address_whitelist" not in self._contracts:
+            self._contracts["address_whitelist"] = self.deployer.deploy(self.project.AddressWhitelistContract)
+        #return self._contracts["address_whitelist"]
+
+    def deploy_identifier_whitelist(self):
+        if "identifier_whitelist" not in self._contracts:
+            self._contracts["identifier_whitelist"] = self.deployer.deploy(self.project.IdentifierWhitelistContract)
+        #return self._contracts["identifier_whitelist"]
+
+    def deploy_mock_oracle(self):
+        if "mock_oracle" not in self._contracts:
+            finder = self._contracts["finder_contract"]
+            self._contracts["mock_oracle"] = self.deployer.deploy(
+                self.project.MockAncillaryContract,
+                finder.address,
+                constants.empty_address,
+            )
+        #return self._contracts["mock_oracle"]
+    
+    def register_contracts(self):
+        store = self._contracts["store_contract"]
+        finder = self._contracts["finder_contract"]
+        whitelist = self._contracts["address_whitelist"]
+        identifier = self._contracts["identifier_whitelist"]
+        mock = self._contracts["mock_oracle"]
+        currency = self._contracts["currency"]
+
+        finder.changeImplementationAddress(
+            constants.Store,
+            store.address,
+            sender=self.deployer
+        )
+        finder.changeImplementationAddress(
+            constants.CollateralWhitelist,
+            whitelist.address,
+            sender=self.deployer
+        )
+        finder.changeImplementationAddress(
+            constants.IdentifierWhitelist,
+            identifier.address,
+            sender=self.deployer
+        )
+        finder.changeImplementationAddress(
+            constants.Oracle,
+            mock.address,
+            sender=self.deployer,
+        )
+        whitelist.addToWhitelist(
+            currency.address,
+            sender=self.deployer,
+        )
+        identifier.addSupportedIdentifier(
+            constants.default_identifier,
+            sender=self.deployer,
+        )
+        final_fee = {"rawValue": int(constants.minimum_bond / 2)}
+        store.setFinalFee(
+            currency.address,
+            final_fee,
+            sender=self.deployer,
+        )
+
+    def deploy_optimistic_oracle_v3(self):
+        if "optimistic_oracle_v3" not in self._contracts:
+
+            finder = self._contracts["finder_contract"]
+            currency = self._contracts["currency"]
+
+            self._contracts["optimistic_oracle_v3"] = self.deployer.deploy(
+                self.project.OOV3,
+                finder.address,
+                currency.address,
+                constants.default_liveness,
+            )
+            finder.changeImplementationAddress(
+                constants.OptimisticOracleV3,
+                self._contracts["optimistic_oracle_v3"].address,
+                sender=self.deployer,
+            )
+        #return self._contracts["optimistic_oracle_v3"]
+
+    def deploy_expanded_token_blueprint(self):
+        if "expanded_token_blueprint" not in self._contracts:
+            self._contracts["expanded_token_blueprint"] = self.deployer.declare(
+                self.project.ExpandedERC20,
+                "Expanded Token",
+                "EXT",
+                18
+            )
+        #return self._contracts["expanded_token_blueprint"]
+
+    def deploy_outcome_token_factory(self):
+        if "factory" not in self._contracts:
+            blueprint = self._contracts["expanded_token_blueprint"]
+            self._contracts["factory"] = self.deployer.deploy(
+                self.project.OutComeTokenFactory,
+                blueprint.contract_address,
+            )
+        #return self._contracts["factory"]
+    
+    def deploy_all(self):
+        self.deploy_finder_contract()
+        self.deploy_store_contract()
+        self.deploy_ancillary_interface()
+        self.deploy_currency()
+        self.deploy_address_whitelist()
+        self.deploy_identifier_whitelist()
+        self.deploy_mock_oracle()
+        self.register_contracts()
+        self.deploy_optimistic_oracle_v3()
+        self.deploy_expanded_token_blueprint()
+        self.deploy_outcome_token_factory()
+    
+    def get_contracts(self):
+        return self._contracts
+
+    
+    def deploy_prediction_market(self):
+        if "prediction_market" not in self._contracts:
+
+            self.deploy_all()
+
+            finder = self._contracts["finder_contract"]
+            whitelist = self._contracts["address_whitelist"]
+            mock = self._contracts["mock_oracle"]
+            currency = self._contracts["currency"]
+            factory = self._contracts["factory"]
+            optimistic_oracle_v3 = self._contracts["optimistic_oracle_v3"]
+            self._contracts["prediction_market"] = self.deployer.deploy(
+                self.project.PredictionMarket,
+                finder.address,
+                whitelist.address,
+                optimistic_oracle_v3.address,
+                mock.address,
+                currency.address,
+                factory.address,
+            )
+            factory.whitelist(self._contracts["prediction_market"].address, sender=self.deployer)
+        return self._contracts["prediction_market"]
 
 @pytest.fixture(scope="session")
-def deployer(accounts):
-    yield accounts[0]
+def owner(accounts):
+    return accounts[0]
 
 @pytest.fixture(scope="session")
 def user_wallet(accounts):
-    yield accounts[1]
+    return accounts[1]
 
 @pytest.fixture(scope="session")
 def asserter_wallet(accounts):
-    yield accounts[2]
+    return accounts[2]
 
+@pytest.fixture(scope="session")
+def other_account(accounts):
+    return accounts[4]
+
+@pytest.fixture(scope="session")
+def sandbox(project, owner):
+    return Sandbox(project, owner)
--- tests/test_market.py
+++ tests/test_market.py
@@ -1,6 +1,73 @@
+import os
+import sys
 import ape
+from ape import project
+from hexbytes import HexBytes
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+from scripts import constants
 
-def test_market_initialization(
-    deployer
+def _decode_logs(receipt):
+    decoded_logs = receipt.decode_logs()
+    for log in decoded_logs:
+        if log.event_name == "MarketInitialized":
+            # extract the market_id
+            _market_id = log.market_id
+
+            # extract the outcome token addresses
+            outcome_token_one = log.outcome1_token
+            outcome_token_two = log.outcome2_token
+            return _market_id, outcome_token_one, outcome_token_two
+
+
+def test_market(
+    owner,
+    user_wallet,
+    asserter_wallet,
+    other_account,
+    sandbox
 ):
-    pass
+    # deploy prediction market
+    market = sandbox.deploy_prediction_market()
+
+    # mint and appprove currency tokens
+    sandbox.get_contracts()['currency'].allocateTo(owner, constants.reward, sender=owner)
+    sandbox.get_contracts()['currency'].approve(market.address, constants.reward, sender=owner)
+
+    # initialize market
+    receipt = market.initialize_market(
+        constants.outcome_one,
+        constants.outcome_two,
+        constants.description,
+        constants.reward,
+        constants.required_bond,
+        sender=owner
+    )
+    assert sandbox.get_contracts()['currency'].balanceOf(market.address) == constants.reward
+    logs = _decode_logs(receipt)
+    assert logs[1] != constants.empty_address
+    assert logs[1] == market.markets(HexBytes(logs[0])).outcome1_token
+
+    # mint and appprove currency tokens
+    sandbox.get_contracts()['currency'].allocateTo(owner, constants.reward, sender=owner)
+    sandbox.get_contracts()['currency'].approve(market.address, constants.reward, sender=owner)
+    with ape.reverts():
+        market.initialize_market(
+            constants.outcome_one,
+            constants.outcome_one,
+            constants.description,
+            constants.reward,
+            constants.required_bond,
+            sender=owner
+        )
+
+
+    # mint and appprove currency tokens
+    sandbox.get_contracts()["currency"].transfer(other_account, constants.reward, sender=owner)
+    sandbox.get_contracts()['currency'].allocateTo(owner, constants.amount, sender=owner)
+    sandbox.get_contracts()['currency'].approve(market.address, constants.amount, sender=owner)
+
+    # create outcome tokens
+    market.create_outcome_tokens(HexBytes(logs[0]), constants.amount, sender=owner)
+    assert project.TestERC20.at((logs[1])).balanceOf(owner) == constants.amount
+    assert project.TestERC20.at((logs[2])).balanceOf(owner) == constants.amount
+    assert sandbox.get_contracts()['currency'].balanceOf(owner) == 0
